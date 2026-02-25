3f7e410fda38e063f809955b97ac4af8408b6775
anees028 <rehmananees028@gmail.com>
2026-02-25 23:37:30 +0100

    Calculation of the character limit and make the button disable if no charcter remaining.

--- src/components/AddNotes.js
+++ src/components/AddNotes.js
@@ -2,19 +2,25 @@ import { useState } from "react";
 
 const AddNote = ({ handleAddNote }) => {
   const [noteText, setNoteText] = useState("");
-  const [remainingChars, setRemainingChars] = useState(200);
   const characterLimit = 200;
+  const [remainingChars, setRemainingChars] = useState(characterLimit);
 
   const handleOnchange = (event) => {
     const inputText = event.target.value;
-    setNoteText(inputText);
     setRemainingChars(characterLimit - inputText.length);
+    if (characterLimit - inputText.length >= 0) {
+      setNoteText(inputText);
+    } else {
+      setRemainingChars(0);
+    }
   };
 
   const handleSaveClick = (event) => {
-    handleAddNote(noteText);
-    event.preventDefault();
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText);
+    }
     setNoteText("");
+    event.preventDefault();
     setRemainingChars(characterLimit);
   };
 
@@ -29,7 +35,11 @@ const AddNote = ({ handleAddNote }) => {
       ></textarea>
       <div className="note-footer">
         <small>{remainingChars} Remaining</small>
-        <button className="save" onClick={handleSaveClick}>
+        <button
+          className="save"
+          onClick={handleSaveClick}
+          disabled={remainingChars < 0}
+        >
           Save
         </button>
       </div>
